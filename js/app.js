9a4544eab0390c3abaa3667596e59a8fc0907b56
DrewDen <denson10@gmail.com>
2019-03-05 19:56:19 -0500

    This commit will update the enemy function.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 550;
+    //this.randomSpeed = this.speed + Math.ceil(Math.random() * 512);
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
